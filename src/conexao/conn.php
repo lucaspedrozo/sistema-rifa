0598a323722eda31e35be99b0ce673aa2a193d9d
Renan-libera <renanhenri12@hotmail.com>
2022-05-16 20:15:11 -0300

    commit

---
+++ src/conexao/conn.php
@@ -0,0 +1,14 @@
+<?php
+
+    $hostname = "sql102.epizy.com";
+    $dbname = "epiz_31454632_rifa";
+    $username = "epiz_31454632";
+    $password = "0wDpvJ3s9MJ";
+
+    try {
+        $pdo = new PDO('mysql:host='.$hostname.';dbname='.$dbname, $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //echo 'Conecao';
+    } catch (PDOException $e) {
+        echo 'Error: '.$e->getMessage();
+    }
